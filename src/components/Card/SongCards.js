1facc9f429214b72974989cb37c683c8da46bef9
ajmal4142 <109949470+ajmal4142@users.noreply.github.com>
2023-10-06 16:58:45 +0530

    Update SongCards.js

--- src/components/Card/SongCards.js
+++ src/components/Card/SongCards.js
@@ -212,7 +212,7 @@ function SongCards({ album }) {
             <button className="faplayclass">
               <FaPlay className="faplay" />
             </button>
-            {selectedSong?.color && !token ? (
+            {selectedSong?.color && token ? (
               <FavoriteIcon
                 onClick={() => handleFavoriteClick(selectedSong)}
                 sx={{
